bbb56d9eadaed3699884e6ffbc03ee3c52e3dc16
Julia-Schl <147834170+Julia-Schl@users.noreply.github.com>
2025-01-12 15:52:18 +0100

    dynamisch

--- otree-example/survey_example_appfolder/models.py
+++ otree-example/survey_example_appfolder/models.py
@@ -17,31 +17,42 @@ doc = 'This is the app for the Candidates Pictures group of the "Designing and i
 class Constants(BaseConstants):
     name_in_url = 'politician-pictures'
     players_per_group = None
-    num_rounds = 1
+    num_rounds = 2
 
-        # Global dictionary for group pictures
+    # Global dictionary for group pictures
     groupPictures = {
         0: ["1", "2"],
         1: ["3", "4"],
         2: ["5", "6"],
         3: ["7", "8"]
     }
-
     
 
 class Subsession(BaseSubsession):
-        
-         def creating_session(self):
 
-            #fetch players
+    def creating_session(self):
+        if self.round_number == 1:
             players = self.get_players()
-            #if we want we can shuffle the players to be more randomly distributed among groups:
-            #random.shuffle(players)
-
-            #randomly assign a group to each participant
-            num_groups = 4 
+            num_groups = 4
             for i, p in enumerate(players):
                 p.group_assignment = i % num_groups
+        
+        else:
+            # Carry over group_assignment from the first round
+            for p in self.get_players():
+                p.group_assignment = p.in_round(1).group_assignment
+        
+         #def creating_session(self):
+
+            # fetch players
+            #players = self.get_players()
+            # if we want we can shuffle the players to be more randomly distributed among groups:
+            # random.shuffle(players)
+
+            # randomly assign a group to each participant
+            #num_groups = 4 
+            #for i, p in enumerate(players):
+                #p.group_assignment = i % num_groups
                 
 
 
@@ -53,9 +64,26 @@ class Group(BaseGroup):
 class Player(BasePlayer):
     #variable for group assignment
     group_assignment = models.IntegerField(initial=-1)
+
+    #variable for picture assignment
+    picture_assignment_1 = models.IntegerField(initial=-1)
+    picture_assignment_2 = models.IntegerField(initial=-1)
     
     #The Variables are structured on the base of pages
-    popout_question_competence = models.IntegerField(
+    popout_question_competence_1 = models.IntegerField(
+        initial=-999,
+        choices =[
+            (1, '1'),
+            (2, '2'),
+            (3, '3'),
+            (4, '4'),
+            (5, '5'),
+            (-1, 'Keine Angabe')
+        ],
+        label="Wie schätzen Sie die Kompetenz dieser Politikerin ein?"
+    )
+
+    popout_question_competence_2 = models.IntegerField(
         initial=-999,
         choices =[
             (1, '1'),
--- otree-example/survey_example_appfolder/pages.py
+++ otree-example/survey_example_appfolder/pages.py
@@ -8,29 +8,72 @@ from .models import Constants, Player
 #more functionality added here but this is a good start. 
 
 class Welcome(Page):
+    #def is_displayed(self):
+        #return self.num_rounds == 1
     pass
 
 class Politician1_competence(Page):
-    form_model = Player
-    form_fields = ["popout_question_competence"]
+    form_model = 'player' 
+
+    # Dynamically set form_fields
+    def get_form_fields(self):
+        # Check the round or instance to decide which field to use
+        if self.round_number == 1:  # Adjust logic based on your setup
+            return ['popout_question_competence_1', 'picture_assignment_1']
+        elif self.round_number == 2:
+            return ['popout_question_competence_2', 'picture_assignment_2']
+
+    # Pass dynamic variables to the template
+    def vars_for_template(self):
+        if self.round_number == 1:
+            return {
+                'field_name': 'popout_question_competence_1',
+                'field_value': self.player.popout_question_competence_1,
+                'picture_field': 'picture_assignment_1',  
+                'current_picture': self.player.picture_assignment_1,
+                'player_group': self.player.group_assignment,  # Group assignment for the player
+                'available_pictures': Constants.groupPictures[self.player.group_assignment],  # Pictures for the player's group
+            }
+        elif self.round_number == 2:
+            return {
+                'field_name': 'popout_question_competence_2',
+                'field_value': self.player.popout_question_competence_2,
+                'picture_field': 'picture_assignment_2', 
+                'current_picture': self.player.picture_assignment_2, 
+                'player_group': self.player.group_assignment,  # Group assignment for the player
+                'available_pictures': Constants.groupPictures[self.player.group_assignment],  # Pictures for the player's group
+            }
+    
+    def is_displayed(self):
+        return True
 
 class Politician2_femininity(Page):
     form_model = Player
     form_fields = ["popout_question_femininity"]
     
-
+    #def is_displayed(self):
+        #return self.num_rounds == 1
 
 class DemoPage(Page):
     form_model = Player
     form_fields = ['age_question']
 
+    #def is_displayed(self):
+        #return self.num_rounds == 1
+
 class EndPage(Page):
     #style: this is a good example of the style 'CamelCase' that one normally uses for classes
     form_model = Player
 
+    #def is_displayed(self):
+        #return self.num_rounds == 1
+
 #Here we define in which ordering we want the pages to be shown. We always start with a Welcome page and end with an End page.
-page_sequence = [Welcome,
-                Politician1_competence,
-                Politician2_femininity,
-                DemoPage,           
-                EndPage]
+#page_sequence = [Welcome,
+                #Politician1_competence,
+                #Politician2_femininity,
+                #DemoPage,           
+                #EndPage]
+
+page_sequence = [Welcome, 
+                Politician1_competence]
