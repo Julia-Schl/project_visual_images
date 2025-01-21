c4f4dbd5efff0149e53ce4a805736c79e4df3452
Richard Neureuther <richard.neureuther@uni-konstanz.de>
2025-01-21 10:50:05 +0100

    Cleanup models page

--- otree-example/survey_example_appfolder/models.py
+++ otree-example/survey_example_appfolder/models.py
@@ -177,10 +177,10 @@ class Player(BasePlayer):
 
     #methods to handle JSON serialization and deserialization (should be in HelperMethods but I cant import them for some reason)
     def set_player_pictures(self, pictures_dict):
-        """Sets the player's picture dictionary."""
+        #Sets the player's picture dictionary.
         self.player_pictures = json.dumps(pictures_dict)
     def get_player_pictures(self):
-        """Returns the player's picture dictionary."""
+        #Returns the player's picture dictionary.
         return json.loads(self.player_pictures)
 
     #Display variables for the Survey
